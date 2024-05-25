ec8fcb40a720a661d8ef4284f4d895c8dd0922e3
Rayan Hnide <eng.rayanhnide98@gmail.com>
2024-05-25 13:21:01 +0300

    Update ReceivedProposal.js

--- src/components/client-dashboard/ReceivedProposal.js
+++ src/components/client-dashboard/ReceivedProposal.js
@@ -1,11 +1,13 @@
-import React from 'react';
+ import React from 'react';
 import { Col, Container, Image, Row } from 'react-bootstrap';
 import Moment from 'react-moment';
 import { useSelector } from 'react-redux';
+import {useRouter} from "next/router";
 // import { IconArrowLeft } from 'tabler-icons';
 
 export default function ReceivedProposal({ style, styles, handleReceivedProposalBackIcon, viewReceivedProposalData, proposedJobsDetails, setViewJobProposal }) {
-    const { id, jobDescription, totalCount } = viewReceivedProposalData;
+    const { id,  jobDescription, totalCount } = viewReceivedProposalData;
+    const router = useRouter()
 
     return (
         <>
@@ -31,6 +33,7 @@ export default function ReceivedProposal({ style, styles, handleReceivedProposal
                         <p className={`${style.jobDescription} mt-0 mb-2`}>
                             {jobDescription}
                         </p>
+
                         <div className={`d-flex pt-1`}>
                             <p className={`${style.proposalsRecieved} my-0 me-2`}>Proposals Recieved - </p>
                             <p className={`${styles.proposalsCount} my-0`}>{totalCount}</p>
@@ -43,15 +46,20 @@ export default function ReceivedProposal({ style, styles, handleReceivedProposal
                 {proposedJobsDetails?.length ?
                     proposedJobsDetails?.map((item, index) => (
                         <div key={index}>
+
+
                             <Row className={`d-flex justify-content-between align-items-center`}>
                                 <Col className='d-flex justify-content-start align-items-center'>
+
                                     <div>
                                         <Image src={`${item?.profileImage ? item?.profileImage : '/images/dummyImage.png'}`} className={`border img img-fluid ${style.getImage} me-3`} alt='job' />
                                     </div>
                                     <div>
                                         <p className={`${style.companyName} my-1`}>
                                             {`${item?.fullName || 'John Smith'}`}
+                                            {item.charges}
                                         </p>
+
                                         <div className='d-flex justify-content-between align-items-center'>
                                             <span className={`d-flex justify-content-center align-items-center me-4 `}>
                                                 <Image src='/images/jobTime.png' className='img img-fluid me-1' alt='time' />
@@ -67,7 +75,9 @@ export default function ReceivedProposal({ style, styles, handleReceivedProposal
                                             </span>
                                         </div>
                                     </div>
+
                                 </Col>
+
                                 <Col md={2} className='text-end'>
                                     {item?.proposalStatus != '1' &&
                                         <p className={`${item?.proposalStatus == '2' ? 'text-success' : 'text-danger'}`}                                        >
@@ -88,6 +98,37 @@ export default function ReceivedProposal({ style, styles, handleReceivedProposal
                                     </p>
                                 </Col>
                             </Row>
+                            <div className='d-flex justify-content-end'>
+                                {(item?.proposalStatus === '2' || item?.proposalStatus === 2) && (
+                                    <button className='mt-3' style={{
+                                        fontWeight: '900',
+                                        fontSize: '14px',
+                                        lineHeight: '60%',
+                                        cursor: 'pointer',
+                                        padding: '13px',
+                                        gap: '8px',
+                                        background: '#0E184D',
+                                        borderRadius: '12px',
+                                        color: '#FFFFFF',
+                                        border: '0',
+                                    }} onClick={() => {
+                                        const itemData = {
+                                            id: item.id,
+                                            jobId: item.jobId,
+                                            talentId: item.talentId,
+                                            talentEmail: item.talentEmail,
+                                            page: false,
+                                            jobName: item.jobName
+                                        };
+
+                                        const queryString = encodeURIComponent(JSON.stringify(itemData));
+                                        router.push(`/client-contract?data=${queryString}`);
+                                        console.log(item.jobName);
+                                    }}>
+                                        Create Contract
+                                    </button>
+                                )}
+                            </div>
                             <hr className={`${styles.hr}`} />
                         </div>
 
@@ -99,5 +140,6 @@ export default function ReceivedProposal({ style, styles, handleReceivedProposal
                 }
             </Container >
         </>
+
     )
 }
